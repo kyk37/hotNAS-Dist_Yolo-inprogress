03befa7765aadcc264366e244587aef8b9ba05d1
kyk37 <kyk37@pitt.edu>
2024-05-23 11:23:12 -0400

    Updated some functions
    Updated  sigmoid_focal_loss, softmax_focal_loss

--- yolo3/loss.py
+++ yolo3/loss.py
@@ -1,12 +1,19 @@
 # -*- coding=utf-8 -*-
 #!/usr/bin/python3
 
+
+## TODO: Convert this from Tensorflow to Pytorch
+
 import math
 import tensorflow as tf
 from tensorflow.keras import backend as K
 from yolo3.postprocess import yolo3_decode
 
-def softmax_focal_loss(y_true, y_pred, gamma=2.0, alpha=0.25):
+import torch
+from torch import nn
+from torch import Tensor
+
+def softmax_local_loss(y_true, y_pred, gamma=2.0, alpha=0.25):
     """
     Compute softmax focal loss.
     Reference Paper:
@@ -24,23 +31,17 @@ def softmax_focal_loss(y_true, y_pred, gamma=2.0, alpha=0.25):
     # Returns
         softmax_focal_loss: Softmax focal loss, tensor of shape (?, num_boxes).
     """
-
-    # Scale predictions so that the class probas of each sample sum to 1
-    #y_pred /= K.sum(y_pred, axis=-1, keepdims=True)
-
-    # Clip the prediction value to prevent NaN's and Inf's
-    #epsilon = K.epsilon()
-    #y_pred = K.clip(y_pred, epsilon, 1. - epsilon)
-    y_pred = tf.nn.softmax(y_pred)
-    y_pred = tf.maximum(tf.minimum(y_pred, 1 - 1e-15), 1e-15)
-
+    y_pred = nn.Softmax(y_pred)
+    y_pred = torch.max(torch.min(y_pred, 1 - 1e-15), 1e-15)
+    
     # Calculate Cross Entropy
-    cross_entropy = -y_true * tf.math.log(y_pred)
-
+    cross_entropy = -y_true * torch.log(y_pred)
+    
     # Calculate Focal Loss
-    softmax_focal_loss = alpha * tf.pow(1 - y_pred, gamma) * cross_entropy
-
+    softmax_focal_loss = alpha * torch.pow(1 - y_pred, gamma) * cross_entropy
+    
     return softmax_focal_loss
+
 
 
 def sigmoid_focal_loss(y_true, y_pred, gamma=2.0, alpha=0.25):
@@ -61,19 +62,20 @@ def sigmoid_focal_loss(y_true, y_pred, gamma=2.0, alpha=0.25):
     # Returns
         sigmoid_focal_loss: Sigmoid focal loss, tensor of shape (?, num_boxes).
     """
-    sigmoid_loss = K.binary_crossentropy(y_true, y_pred, from_logits=True)
+    sigmoid_loss = nn.BCELoss(y_true, y_pred, from_logits=True)
 
-    pred_prob = tf.sigmoid(y_pred)
+    pred_prob = nn.Sigmoid(y_pred)
     p_t = ((y_true * pred_prob) + ((1 - y_true) * (1 - pred_prob)))
-    modulating_factor = tf.pow(1.0 - p_t, gamma)
+    modulating_factor = torch.pow(1.0 - p_t, gamma)
     alpha_weight_factor = (y_true * alpha + (1 - y_true) * (1 - alpha))
 
     sigmoid_focal_loss = modulating_factor * alpha_weight_factor * sigmoid_loss
-    #sigmoid_focal_loss = tf.reduce_sum(sigmoid_focal_loss, axis=-1)
+    #sigmoid_focal_loss = torch.sum(sigmoid_focal_loss, axis=-1)
 
     return sigmoid_focal_loss
 
 
+#TODO
 def box_iou(b1, b2):
     """
     Return iou tensor
@@ -112,6 +114,114 @@ def box_iou(b1, b2):
     iou = intersect_area / (b1_area + b2_area - intersect_area)
 
     return iou
+
+# def softmax_focal_loss(y_true, y_pred, gamma=2.0, alpha=0.25):
+#     """
+#     Compute softmax focal loss.
+#     Reference Paper:
+#         "Focal Loss for Dense Object Detection"
+#         https://arxiv.org/abs/1708.02002
+
+#     # Arguments
+#         y_true: Ground truth targets,
+#             tensor of shape (?, num_boxes, num_classes).
+#         y_pred: Predicted logits,
+#             tensor of shape (?, num_boxes, num_classes).
+#         gamma: exponent of the modulating factor (1 - p_t) ^ gamma.
+#         alpha: optional alpha weighting factor to balance positives vs negatives.
+
+#     # Returns
+#         softmax_focal_loss: Softmax focal loss, tensor of shape (?, num_boxes).
+#     """
+
+#     # Scale predictions so that the class probas of each sample sum to 1
+#     #y_pred /= K.sum(y_pred, axis=-1, keepdims=True)
+
+#     # Clip the prediction value to prevent NaN's and Inf's
+#     #epsilon = K.epsilon()
+#     #y_pred = K.clip(y_pred, epsilon, 1. - epsilon)
+#     y_pred = tf.nn.softmax(y_pred)
+#     y_pred = tf.maximum(tf.minimum(y_pred, 1 - 1e-15), 1e-15)
+
+#     # Calculate Cross Entropy
+#     cross_entropy = -y_true * tf.math.log(y_pred)
+
+#     # Calculate Focal Loss
+#     softmax_focal_loss = alpha * tf.pow(1 - y_pred, gamma) * cross_entropy
+
+#     return softmax_focal_loss
+
+
+# def sigmoid_focal_loss(y_true, y_pred, gamma=2.0, alpha=0.25):
+#     """
+#     Compute sigmoid focal loss.
+#     Reference Paper:
+#         "Focal Loss for Dense Object Detection"
+#         https://arxiv.org/abs/1708.02002
+
+#     # Arguments
+#         y_true: Ground truth targets,
+#             tensor of shape (?, num_boxes, num_classes).
+#         y_pred: Predicted logits,
+#             tensor of shape (?, num_boxes, num_classes).
+#         gamma: exponent of the modulating factor (1 - p_t) ^ gamma.
+#         alpha: optional alpha weighting factor to balance positives vs negatives.
+
+#     # Returns
+#         sigmoid_focal_loss: Sigmoid focal loss, tensor of shape (?, num_boxes).
+#     """
+#     sigmoid_loss = K.binary_crossentropy(y_true, y_pred, from_logits=True)
+
+#     pred_prob = tf.sigmoid(y_pred)
+#     p_t = ((y_true * pred_prob) + ((1 - y_true) * (1 - pred_prob)))
+#     modulating_factor = tf.pow(1.0 - p_t, gamma)
+#     alpha_weight_factor = (y_true * alpha + (1 - y_true) * (1 - alpha))
+
+#     sigmoid_focal_loss = modulating_factor * alpha_weight_factor * sigmoid_loss
+#     #sigmoid_focal_loss = tf.reduce_sum(sigmoid_focal_loss, axis=-1)
+
+#     return sigmoid_focal_loss
+
+
+
+# def box_iou(b1, b2):
+#     """
+#     Return iou tensor
+
+#     Parameters
+#     ----------
+#     b1: tensor, shape=(i1,...,iN, 4), xywh
+#     b2: tensor, shape=(j, 4), xywh
+
+#     Returns
+#     -------
+#     iou: tensor, shape=(i1,...,iN, j)
+#     """
+#     # Expand dim to apply broadcasting.
+#     b1 = K.expand_dims(b1, -2)
+#     b1_xy = b1[..., :2]
+#     b1_wh = b1[..., 2:4]
+#     b1_wh_half = b1_wh/2.
+#     b1_mins = b1_xy - b1_wh_half
+#     b1_maxes = b1_xy + b1_wh_half
+
+#     # Expand dim to apply broadcasting.
+#     b2 = K.expand_dims(b2, 0)
+#     b2_xy = b2[..., :2]
+#     b2_wh = b2[..., 2:4]
+#     b2_wh_half = b2_wh/2.
+#     b2_mins = b2_xy - b2_wh_half
+#     b2_maxes = b2_xy + b2_wh_half
+
+#     intersect_mins = K.maximum(b1_mins, b2_mins)
+#     intersect_maxes = K.minimum(b1_maxes, b2_maxes)
+#     intersect_wh = K.maximum(intersect_maxes - intersect_mins, 0.)
+#     intersect_area = intersect_wh[..., 0] * intersect_wh[..., 1]
+#     b1_area = b1_wh[..., 0] * b1_wh[..., 1]
+#     b2_area = b2_wh[..., 0] * b2_wh[..., 1]
+#     iou = intersect_area / (b1_area + b2_area - intersect_area)
+
+#     return iou
 
 
 def box_giou(b_true, b_pred):
