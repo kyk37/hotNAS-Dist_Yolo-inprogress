100179408a3abbdd4fe82db7fbc92ddb1f6a7ade
kyk37@pitt.edu <kyk37@pitt.edu>
2024-06-05 00:38:21 -0400

    Train & Utils
    - Made changes to Utils for multiple GPU support with function optimize_tf_gpu(model)
    - Started conversion of Keras includes like Tensorboard, ModelCheckpoint, Earlystopping to pytorch lightning as it's the easiest equivalent.

--- common/utils.py
+++ common/utils.py
@@ -12,29 +12,40 @@ from matplotlib.colors import rgb_to_hsv, hsv_to_rgb
 from common.backbones.efficientnet import swish
 from common.backbones.mobilenet_v3 import hard_sigmoid, hard_swish
 from yolo4.models.layers import mish
-import tensorflow as tf
+#import tensorflow as tf
+
+import torch
+from torch import nn
+
+def optimize_tf_gpu(model):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    #enable all GPUs to be used
+    model = nn.DataParallel(model)
+    # model = nn.DataParallel(model, device_ids = [0,1])
+    model.to(device)
+
+# def optimize_tf_gpu(tf, K):
+#     if tf.__version__.startswith('2'):
+#         gpus = tf.config.experimental.list_physical_devices('GPU')
+#         if gpus:
+#             try:
+#                 # Currently, memory growth needs to be the same across GPUs
+#                 for gpu in gpus:
+#                     # tf.config.experimental.set_virtual_device_configuration(gpu, [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=10000)])
+#                     tf.config.experimental.set_memory_growth(gpu, True)
+#             except RuntimeError as e:
+#                 # Memory growth must be set before GPUs have been initialized
+#                 print(e)
+#     else:
+#         config = tf.ConfigProto()
+#         config.gpu_options.allow_growth = True  # dynamic alloc GPU resource
+#         config.gpu_options.per_process_gpu_memory_fraction = 0.9  # GPU memory threshold 0.3
+#         session = tf.Session(config=config)
+
+#         # set session
+#         K.set_session(session)
 
 
-def optimize_tf_gpu(tf, K):
-    if tf.__version__.startswith('2'):
-        gpus = tf.config.experimental.list_physical_devices('GPU')
-        if gpus:
-            try:
-                # Currently, memory growth needs to be the same across GPUs
-                for gpu in gpus:
-                    # tf.config.experimental.set_virtual_device_configuration(gpu, [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=10000)])
-                    tf.config.experimental.set_memory_growth(gpu, True)
-            except RuntimeError as e:
-                # Memory growth must be set before GPUs have been initialized
-                print(e)
-    else:
-        config = tf.ConfigProto()
-        config.gpu_options.allow_growth = True  # dynamic alloc GPU resource
-        config.gpu_options.per_process_gpu_memory_fraction = 0.9  # GPU memory threshold 0.3
-        session = tf.Session(config=config)
-
-        # set session
-        K.set_session(session)
 
 
 def get_custom_objects():
--- train.py
+++ train.py
@@ -3,11 +3,18 @@
 """
 Retrain the YOLO model for your own dataset.
 """
+
+###
+### run "pip install torch_tb_profiler"
+### 
+
+
 import os, time, random, argparse
 import numpy as np
-import tensorflow.keras.backend as K
+
+#import tensorflow.keras.backend as K
 #FIXME from tensorflow.keras.utils import multi_gpu_model
-from tensorflow.keras.callbacks import TensorBoard, ModelCheckpoint, ReduceLROnPlateau, LearningRateScheduler, EarlyStopping, TerminateOnNaN, LambdaCallback
+#from tensorflow.keras.callbacks import TensorBoard, ModelCheckpoint, ReduceLROnPlateau, LearningRateScheduler, EarlyStopping, TerminateOnNaN, LambdaCallback
 #FIXME from tensorflow_model_optimization.sparsity import keras as sparsity
 
 from yolo3.model import get_yolo3_train_model
@@ -23,11 +30,19 @@ from datetime import datetime
 os.environ['TF_ENABLE_AUTO_MIXED_PRECISION'] = '1'
 os.environ['TF_AUTO_MIXED_PRECISION_GRAPH_REWRITE_IGNORE_PERFORMANCE'] = '1'
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+#import tensorflow as tf
 
-import tensorflow as tf
-optimize_tf_gpu(tf, K)
+import torch
+import pytorch_lightning as pl
+from torch import optim
+from pytorch_lightning.loggers import TensorBoardLogger
+from pytorch_lightning.profilers import PyTorchProfiler
+from torch.profiler import profile, record_function, ProfilerActivity
+from pytorch_lightning import Trainer
+from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
 
 
+#optimize_tf_gpu(model)
 def main(args):
     dataset_working_directory = args.dataset_working_directory
     annotation_file = args.annotation_file
@@ -51,19 +66,41 @@ def main(args):
     freeze_level = 0
 
     # callbacks for training process
-    logging = TensorBoard(log_dir=log_dir, histogram_freq=0, write_graph=False, write_grads=False, write_images=False, update_freq='batch')
-    checkpoint = ModelCheckpoint(os.path.join(log_dir, 'ep{epoch:03d}-loss{loss:.3f}-val_loss{val_loss:.3f}.h5'),
-        monitor='val_loss',
-        mode='min',
-        verbose=1,
-        save_weights_only=False,
-        save_best_only=False,
-        period=args.checkpoint_period)
-    reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.5, mode='min', patience=10, verbose=1, cooldown=0, min_lr=1e-10)
-    early_stopping = EarlyStopping(monitor='val_loss', min_delta=0, patience=50, verbose=1, mode='min')
-    terminate_on_nan = TerminateOnNaN()
+    # logging = TensorBoard(log_dir=log_dir, histogram_freq=0, write_graph=False, write_grads=False, write_images=False, update_freq='batch')
+    # checkpoint = ModelCheckpoint(os.path.join(log_dir, 'ep{epoch:03d}-loss{loss:.3f}-val_loss{val_loss:.3f}.h5'),
+    #     monitor='val_loss',
+    #     mode='min',
+    #     verbose=1,
+    #     save_weights_only=False,
+    #     save_best_only=False,
+    #     period=args.checkpoint_period)
+    
+    # early_stopping = EarlyStopping(monitor='val_loss', min_delta=0, patience=50, verbose=1, mode='min')
+    # terminate_on_nan = TerminateOnNaN()
+    # callbacks=[logging, checkpoint, reduce_lr, early_stopping, terminate_on_nan]
 
-    callbacks=[logging, checkpoint, reduce_lr, early_stopping, terminate_on_nan]
+    
+    #https://lightning.ai/docs/pytorch/stable/api/lightning.pytorch.loggers.tensorboard.html#module-lightning.pytorch.loggers.tensorboard
+    logging = TensorBoardLogger(save_dir=log_dir, name="dist-Yolo")
+    profiler = PyTorchProfiler(
+        # may need? on_trace_ready = torch.profiler.tensorboard_trace_handler("tb_logs/profiler") 
+        dirpath = log_dir,
+        schedule= torch.profiler. schedule(skip_first=10, wait=1, warmup=1,active=20)
+    )
+    checkpoint = ModelCheckpoint(os.path.join(log_dir, 'ep{epoch:03d}-loss{loss:.3f}-val_loss{val_loss:.3f}.h5'),
+                                 monitor = 'val_loss',
+                                 verbose = 1,
+                                 save_weights_only=False,
+                                 mode = 'min',
+                                 every_n_train_steps=args.checkpoint_period) 
+    optim
+    reduce_lr = optim.ReduceLROnPlateau(monitor='val_loss', factor=0.5, mode='min', patience=10, verbose=1, cooldown=0, min_lr=1e-10)
+    early_stopping = EarlyStopping(monitor="val_loss", min_delta = 0, patience = 50, verbose = 1, mode="min")
+
+
+    #https://lightning.ai/docs/pytorch/1.6.0/api/pytorch_lightning.trainer.trainer.Trainer.html
+    trainer = Trainer(profiler=profiler, logger=logging, detect_anomaly=True) #Detect anomoly is "TerminateOnNan"
+    callbacks=[logging, checkpoint, reduce_lr, early_stopping]
 
     # get train&val dataset
     dataset = get_dataset(annotation_file, dataset_working_directory)
@@ -164,18 +201,18 @@ def main(args):
     print("Transfer training stage")
     print('Train on {} samples, val on {} samples, with batch size {}, input_shape {}.'.format(num_train, num_val, args.batch_size, input_shape))
     #model.fit_generator(train_data_generator,
-    model.fit_generator(data_generator(dataset[:num_train], args.batch_size, input_shape, anchors, num_classes, args.enhance_augment, rescale_interval, multi_anchor_assign=args.multi_anchor_assign),
-            steps_per_epoch=max(1, num_train//args.batch_size),
-            #validation_data=val_data_generator,
-            validation_data=data_generator(dataset[num_train:], args.batch_size, input_shape, anchors, num_classes, multi_anchor_assign=args.multi_anchor_assign),
-            validation_steps=max(1, num_val//args.batch_size),
-            epochs=epochs,
-            initial_epoch=initial_epoch,
-            #verbose=1,
-            workers=1,
-            use_multiprocessing=False,
-            max_queue_size=10,
-            callbacks=callbacks)
+    # model.fit_generator(data_generator(dataset[:num_train], args.batch_size, input_shape, anchors, num_classes, args.enhance_augment, rescale_interval, multi_anchor_assign=args.multi_anchor_assign),
+    #         steps_per_epoch=max(1, num_train//args.batch_size),
+    #         #validation_data=val_data_generator,
+    #         validation_data=data_generator(dataset[num_train:], args.batch_size, input_shape, anchors, num_classes, multi_anchor_assign=args.multi_anchor_assign),
+    #         validation_steps=max(1, num_val//args.batch_size),
+    #         epochs=epochs,
+    #         initial_epoch=initial_epoch,
+    #         #verbose=1,
+    #         workers=1,
+    #         use_multiprocessing=False,
+    #         max_queue_size=10,
+    #         callbacks=callbacks)
 
     # Wait 2 seconds for next stage
     time.sleep(2)
@@ -204,26 +241,27 @@ def main(args):
 
     print('Train on {} samples, val on {} samples, with batch size {}, input_shape {}.'.format(num_train, num_val, args.batch_size, input_shape))
     #model.fit_generator(train_data_generator,
-    model.fit_generator(data_generator(dataset[:num_train], args.batch_size, input_shape, anchors, num_classes, args.enhance_augment, rescale_interval, multi_anchor_assign=args.multi_anchor_assign),
-        steps_per_epoch=max(1, num_train//args.batch_size),
-        #validation_data=val_data_generator,
-        validation_data=data_generator(dataset[num_train:], args.batch_size, input_shape, anchors, num_classes, multi_anchor_assign=args.multi_anchor_assign),
-        validation_steps=max(1, num_val//args.batch_size),
-        epochs=args.total_epoch,
-        initial_epoch=epochs,
-        #verbose=1,
-        workers=1,
-        use_multiprocessing=False,
-        max_queue_size=10,
-        callbacks=callbacks)
+    # model.fit_generator(data_generator(dataset[:num_train], args.batch_size, input_shape, anchors, num_classes, args.enhance_augment, rescale_interval, multi_anchor_assign=args.multi_anchor_assign),
+    #     steps_per_epoch=max(1, num_train//args.batch_size),
+    #     #validation_data=val_data_generator,
+    #     validation_data=data_generator(dataset[num_train:], args.batch_size, input_shape, anchors, num_classes, multi_anchor_assign=args.multi_anchor_assign),
+    #     validation_steps=max(1, num_val//args.batch_size),
+    #     epochs=args.total_epoch,
+    #     initial_epoch=epochs,
+    #     #verbose=1,
+    #     workers=1,
+    #     use_multiprocessing=False,
+    #     max_queue_size=10,
+    #     callbacks=callbacks)
+
+
 
     # Finally store model
     if args.model_pruning:
         #FIXME model = sparsity.strip_pruning(model)
         raise "Not resolved, not supported now"
     model.save(os.path.join(log_dir, 'trained_final.h5'))
-
-
+    
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -303,3 +341,202 @@ if __name__ == '__main__':
     args.model_image_size = (int(height), int(width))
 
     main(args)
+
+
+
+
+
+# def main(args):
+#     dataset_working_directory = args.dataset_working_directory
+#     annotation_file = args.annotation_file
+#     annotation_file = os.path.join(dataset_working_directory, annotation_file)
+#     log_dir = os.path.join(args.log_dir, datetime.now().strftime("%Y_%m_%d_%H_%M_%S"))
+#     classes_path = args.classes_path
+#     class_names = get_classes(classes_path)
+#     num_classes = len(class_names)
+
+#     anchors = get_anchors(args.anchors_path)
+#     num_anchors = len(anchors)
+
+#     # get freeze level according to CLI option
+#     if args.weights_path:
+#         freeze_level = 0
+#     else:
+#         freeze_level = 1
+
+#     if args.freeze_level is not None:
+#         freeze_level = args.freeze_level
+#     freeze_level = 0
+
+#     # callbacks for training process
+#     logging = TensorBoard(log_dir=log_dir, histogram_freq=0, write_graph=False, write_grads=False, write_images=False, update_freq='batch')
+#     checkpoint = ModelCheckpoint(os.path.join(log_dir, 'ep{epoch:03d}-loss{loss:.3f}-val_loss{val_loss:.3f}.h5'),
+#         monitor='val_loss',
+#         mode='min',
+#         verbose=1,
+#         save_weights_only=False,
+#         save_best_only=False,
+#         period=args.checkpoint_period)
+#     reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.5, mode='min', patience=10, verbose=1, cooldown=0, min_lr=1e-10)
+#     early_stopping = EarlyStopping(monitor='val_loss', min_delta=0, patience=50, verbose=1, mode='min')
+#     terminate_on_nan = TerminateOnNaN()
+
+#     callbacks=[logging, checkpoint, reduce_lr, early_stopping, terminate_on_nan]
+
+#     # get train&val dataset
+#     dataset = get_dataset(annotation_file, dataset_working_directory)
+#     if args.val_annotation_file:
+#         val_dataset = get_dataset(args.val_annotation_file, dataset_working_directory)
+#         num_train = len(dataset)
+#         num_val = len(val_dataset)
+#         dataset.extend(val_dataset)
+#     else:
+#         val_split = args.val_split
+#         num_val = int(len(dataset)*val_split)
+#         num_train = len(dataset) - num_val
+
+#     # assign multiscale interval
+#     if args.multiscale:
+#         rescale_interval = args.rescale_interval
+#     else:
+#         rescale_interval = -1  #Doesn't rescale
+
+#     # model input shape check
+#     input_shape = args.model_image_size
+#     assert (input_shape[0]%32 == 0 and input_shape[1]%32 == 0), 'model_image_size should be multiples of 32'
+
+#     # get different model type & train&val data generator
+#     if num_anchors == 9:
+#         # YOLOv3 use 9 anchors
+#         get_train_model = get_yolo3_train_model
+#         data_generator = yolo3_data_generator_wrapper
+
+#         # tf.keras.Sequence style data generator
+#         #train_data_generator = Yolo3DataGenerator(dataset[:num_train], args.batch_size, input_shape, anchors, num_classes, args.enhance_augment, rescale_interval, args.multi_anchor_assign)
+#         #val_data_generator = Yolo3DataGenerator(dataset[num_train:], args.batch_size, input_shape, anchors, num_classes, multi_anchor_assign=args.multi_anchor_assign)
+
+#         tiny_version = False
+#     elif num_anchors == 6:
+#         # Tiny YOLOv3 use 6 anchors
+#         get_train_model = get_yolo3_train_model
+#         data_generator = yolo3_data_generator_wrapper
+
+#         # tf.keras.Sequence style data generator
+#         #train_data_generator = Yolo3DataGenerator(dataset[:num_train], args.batch_size, input_shape, anchors, num_classes, args.enhance_augment, rescale_interval, args.multi_anchor_assign)
+#         #val_data_generator = Yolo3DataGenerator(dataset[num_train:], args.batch_size, input_shape, anchors, num_classes, multi_anchor_assign=args.multi_anchor_assign)
+
+#         tiny_version = True
+#     elif num_anchors == 5:
+#         # YOLOv2 use 5 anchors
+#         get_train_model = get_yolo2_train_model
+#         data_generator = yolo2_data_generator_wrapper
+
+#         # tf.keras.Sequence style data generator
+#         #train_data_generator = Yolo2DataGenerator(dataset[:num_train], args.batch_size, input_shape, anchors, num_classes, args.enhance_augment, rescale_interval)
+#         #val_data_generator = Yolo2DataGenerator(dataset[num_train:], args.batch_size, input_shape, anchors, num_classes)
+
+#         tiny_version = False
+#     else:
+#         raise ValueError('Unsupported anchors number')
+
+#     # prepare online evaluation callback
+#     if args.eval_online:
+#         eval_callback = EvalCallBack(args.model_type, dataset[num_train:], anchors, class_names, args.model_image_size, args.model_pruning, log_dir, eval_epoch_interval=args.eval_epoch_interval, save_eval_checkpoint=args.save_eval_checkpoint, elim_grid_sense=args.elim_grid_sense)
+#         callbacks.append(eval_callback)
+
+#     # prepare train/val data shuffle callback
+#     if args.data_shuffle:
+#         shuffle_callback = DatasetShuffleCallBack(dataset)
+#         callbacks.append(shuffle_callback)
+
+#     # prepare model pruning config
+#     pruning_end_step = np.ceil(1.0 * num_train / args.batch_size).astype(np.int32) * args.total_epoch
+#     if args.model_pruning:
+#         #FIXME pruning_callbacks = [sparsity.UpdatePruningStep(), sparsity.PruningSummaries(log_dir=log_dir, profile_batch=0)]
+#         #FIXME callbacks = callbacks + pruning_callbacks
+#         raise "Not resolved, not supported now"
+
+#     # prepare optimizer
+#     optimizer = get_optimizer(args.optimizer, args.learning_rate, decay_type=None)  # args.learning_rate, decay_type=None)
+#     optimizer.clipnorm = True
+
+#     # support multi-gpu training
+#     if args.gpu_num >= 2:
+#         # devices_list=["/gpu:0", "/gpu:1"]
+#         devices_list=["/gpu:{}".format(n) for n in range(args.gpu_num)]
+#         strategy = tf.distribute.MirroredStrategy(devices=devices_list)
+#         print ('Number of devices: {}'.format(strategy.num_replicas_in_sync))
+#         with strategy.scope():
+#             # get multi-gpu train model
+#             model = get_train_model(args.model_type, anchors, num_classes, weights_path=args.weights_path, freeze_level=freeze_level, optimizer=optimizer, label_smoothing=args.label_smoothing, elim_grid_sense=args.elim_grid_sense, model_pruning=args.model_pruning, pruning_end_step=pruning_end_step)
+
+#     else:
+#         # get normal train model
+#         model = get_train_model(args.model_type, anchors, num_classes, weights_path=args.weights_path, freeze_level=freeze_level, optimizer=optimizer, label_smoothing=args.label_smoothing, elim_grid_sense=args.elim_grid_sense, model_pruning=args.model_pruning, pruning_end_step=pruning_end_step)
+
+#     model.summary()
+
+#     # Transfer training some epochs with frozen layers first if needed, to get a stable loss.
+#     initial_epoch = args.init_epoch
+#     epochs = initial_epoch + args.transfer_epoch
+#     print("Transfer training stage")
+#     print('Train on {} samples, val on {} samples, with batch size {}, input_shape {}.'.format(num_train, num_val, args.batch_size, input_shape))
+#     #model.fit_generator(train_data_generator,
+#     model.fit_generator(data_generator(dataset[:num_train], args.batch_size, input_shape, anchors, num_classes, args.enhance_augment, rescale_interval, multi_anchor_assign=args.multi_anchor_assign),
+#             steps_per_epoch=max(1, num_train//args.batch_size),
+#             #validation_data=val_data_generator,
+#             validation_data=data_generator(dataset[num_train:], args.batch_size, input_shape, anchors, num_classes, multi_anchor_assign=args.multi_anchor_assign),
+#             validation_steps=max(1, num_val//args.batch_size),
+#             epochs=epochs,
+#             initial_epoch=initial_epoch,
+#             #verbose=1,
+#             workers=1,
+#             use_multiprocessing=False,
+#             max_queue_size=10,
+#             callbacks=callbacks)
+
+#     # Wait 2 seconds for next stage
+#     time.sleep(2)
+
+#     if args.decay_type:
+#         # rebuild optimizer to apply learning rate decay, only after
+#         # unfreeze all layers
+#         callbacks.remove(reduce_lr)
+#         steps_per_epoch = max(1, num_train//args.batch_size)
+#         decay_steps = steps_per_epoch * (args.total_epoch - args.init_epoch - args.transfer_epoch)
+#         optimizer = get_optimizer(args.optimizer, args.learning_rate, decay_type=args.decay_type, decay_steps=decay_steps)
+
+#     # Unfreeze the whole network for further tuning
+#     # NOTE: more GPU memory is required after unfreezing the body
+#     print("Unfreeze and continue training, to fine-tune.")
+#     if args.gpu_num >= 2:
+#         with strategy.scope():
+#             for i in range(len(model.layers)):
+#                 model.layers[i].trainable = True
+#             model.compile(optimizer=optimizer, loss={'yolo_loss': lambda y_true, y_pred: y_pred}) # recompile to apply the change
+
+#     else:
+#         for i in range(len(model.layers)):
+#             model.layers[i].trainable = True
+#         model.compile(optimizer=optimizer, loss={'yolo_loss': lambda y_true, y_pred: y_pred}) # recompile to apply the change
+
+#     print('Train on {} samples, val on {} samples, with batch size {}, input_shape {}.'.format(num_train, num_val, args.batch_size, input_shape))
+#     #model.fit_generator(train_data_generator,
+#     model.fit_generator(data_generator(dataset[:num_train], args.batch_size, input_shape, anchors, num_classes, args.enhance_augment, rescale_interval, multi_anchor_assign=args.multi_anchor_assign),
+#         steps_per_epoch=max(1, num_train//args.batch_size),
+#         #validation_data=val_data_generator,
+#         validation_data=data_generator(dataset[num_train:], args.batch_size, input_shape, anchors, num_classes, multi_anchor_assign=args.multi_anchor_assign),
+#         validation_steps=max(1, num_val//args.batch_size),
+#         epochs=args.total_epoch,
+#         initial_epoch=epochs,
+#         #verbose=1,
+#         workers=1,
+#         use_multiprocessing=False,
+#         max_queue_size=10,
+#         callbacks=callbacks)
+
+#     # Finally store model
+#     if args.model_pruning:
+#         #FIXME model = sparsity.strip_pruning(model)
+#         raise "Not resolved, not supported now"
+#     model.save(os.path.join(log_dir, 'trained_final.h5'))
